708f07ad08459166b2aba15ddc6c8c7319c474f4
User <vasu7120@gmail.com>
2023-01-27 23:25:05 +0530

    adding files

--- src/main/java/net/javaguides/springboot/web/MainController.java
+++ src/main/java/net/javaguides/springboot/web/MainController.java
@@ -3,6 +3,7 @@ package net.javaguides.springboot.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class MainController {
 	
